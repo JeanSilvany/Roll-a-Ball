3c503bd6e009d70a3068256a0e625359a482dbee
JeanSilvany <jeansilvany@hotmail.com>
2026-05-17 01:08:24 -0300

    many additions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,31 +1,80 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+    public TextMeshProUGUI winTextObject;
+    public Image winImage;
+
+    private int count;
     private Rigidbody rb;
     private float movementX;
     private float movementY;
-
-    public float speed = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        winTextObject.gameObject.SetActive(false);
+        winImage.gameObject.SetActive(false);
+        SetCountText();
     }
 
-    void OnMove(InputValue inputValue){
+    void OnMove(InputValue inputValue)
+    {
         Vector2 movementVector = inputValue.Get<Vector2>();
 
         movementX = movementVector.x;
         movementY = movementVector.y;
     }
 
-    void FixedUpdate() {
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= 12)
+        {
+            winTextObject.gameObject.SetActive(true);
+            winImage.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            // Destroy the current object
+
+            // Update the winText to display "You Lose!"
+            winTextObject.gameObject.SetActive(true);
+            winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
+
+            Destroy(gameObject);
+        }
+    }
+
+    void FixedUpdate()
+    {
         Vector3 movement = new(movementX, 0f, movementY);
 
         rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+
+            SetCountText();
+        }
     }
 }
